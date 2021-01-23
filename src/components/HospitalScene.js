d25145142b9572b23450fd4c153754fd6b690426
Zongo Maqutu <MQTZON001@myuct.ac.za>
2021-01-23 11:35:59 +0200

    Added gltf model to my the scene, fix VR camera

--- src/components/HospitalScene.js
+++ src/components/HospitalScene.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState, useRef, Suspense } from 'react';
+import { OrbitControls, useGLTFLoader } from 'drei';
+import { Canvas, useFrame, useThree } from 'react-three-fiber';
+import { VRCanvas, DefaultXRControllers } from 'react-xr';
+import Model from './Model';
 
 function HospitalScene() {
+    let width = window.innerWidth;
+    let height = window.innerHeigh; 
+
+    const cameraPosition = useRef([0,1,0]);
     return (
-        <h1>Lets get this project started</h1>
+        <>
+            <VRCanvas
+            colorManagement
+            camera={
+                {
+                    position: cameraPosition.current,
+                    fov: 52.5,
+                    aspect: width / height, 
+                    far: 5000
+                    }
+                }
+            >
+                <Suspense fallback = {null}>
+                    <mesh visible position = {[0,0,0]}>
+                        <Model/>
+                    </mesh>
+                </Suspense>
+                <OrbitControls />
+                <DefaultXRControllers/>
+            </VRCanvas>
+        </>
     )
 }
 
